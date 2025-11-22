d065ab5171152c548a82d92f6e56406526fb38c1
korual-ops <kys94607712@gmail.com>
2025-11-22 21:05:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ★★ 구글 스프레드시트 WebApp URL ★★
-const API_URL = "https://script.google.com/macros/s/AKfycbx3s5j7YgqcWLGGGuzdtQy0Ayl3QHtHP7xwhEAv3N-BClUVFNZy2krd4WNyOy-kiQE/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwXGKkXsf_C9uTc1o4byZ4SJwUZIkYwGoukARMqbY-Qt7VOwZKqvo0Gph2VSsRJfL5K/exec";
 
 // 시트 이름 (시트 탭 이름 그대로)
 const SHEET_NAME = "시트1";   // ← 실제 너의 스프레드시트 시트 이름으로 변경!
@@ -30,3 +30,4 @@ async function loadSheetData() {
 
 // 페이지 로드 시 자동 실행
 window.onload = loadSheetData;
+
